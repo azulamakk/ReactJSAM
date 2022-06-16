8e18e44c078b413baeb41712e81039696e4e12cd
azulamakk <92961593+azulamakk@users.noreply.github.com>
2022-06-16 01:23:28 -0300

    clase 4

---
+++ Components/Input/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../style.css';
+
+const Input = ({valorPlaceholder, title}) => {
+    return (
+        <input
+            placeholder={`${valorPlaceholder} ${title}`}
+            className="app-input"
+        />
+    )
+}
+
+export default Input;
