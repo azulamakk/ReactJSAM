f508d1d4bf8316096e618df178038c58757a6431
azulamakk <92961593+azulamakk@users.noreply.github.com>
2022-06-27 18:13:02 -0300

    inicio d4

--- src/Containers/ItemListContainer/index.jsx
+++ src/Containers/ItemListContainer/index.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
+import { useState } from 'react';
 // import yerbaMate from '../../assets/yerba-mate.webp';
 
+//const ItemListContainer = ({greeting, children}) => {
+//  return (
+//    <div>
+//        {/* {children} */}
+//        <p>{greeting}</p>
+//        <img src='./assets/tiendaFisica.jpeg' alt="disquera"/>
+//    </div>
+//  )
+//}
+
 const ItemListContainer = ({greeting, children}) => {
-  return (
+  const [color, setColor] = useState ("brown");
+
+  const cambiarColor = () => {
+//    color = "red";
+//    console.log(color)
+    setColor("navy")
+  }
+
+  return(
     <div>
-        {/* {children} */}
-        <p>{greeting}</p>
-        <img src='/assets/yerba-mate.webp' alt="yerba"/>
+      {/* {children} */}
+      <p>{greeting}</p>
+      <p>{color}</p>
+      <button> onClick={cambiarColor}
+        Cambair el color a red
+      </button>
     </div>
   )
 }
