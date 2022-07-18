25e791f2425a3bfc8c8b3fb6498df10363cabeb6
azulamakk <92961593+azulamakk@users.noreply.github.com>
2022-07-17 21:12:01 -0300

    correcciones x pt2

---
+++ src/containers/Cart/index.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react'
+import { Shop } from '././context/ShopContext'
+
+const Cart = () => {
+  const {cart} = useContext(Shop);
+
+  console.log(cart);
+  return (
+    <ul>
+      {cart.map(producto => {
+        return <li key={producto.id}>{producto.nombre} <img src={producto.img} width='80px' alt={producto.nombre}/></li>
+      })}
+    </ul>
+  )
+}
+
+export default Cart
