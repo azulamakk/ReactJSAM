5bd2354911b5c3f97b5df926482c9b0e0075cabc
azulamakk <92961593+azulamakk@users.noreply.github.com>
2022-07-19 20:31:16 -0300

    index fix correcciones xi

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import './App.css';
 import React from 'react';
+import NavBar from './components/NavBar';
+import ItemListContainer from './containers/ItemListContainer';
+import ItemDetailContainer from './containers/ItemDetailContainer'
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import NotFound from './components/NotFound';
+import Cart from './containers/Cart'
 import CounterContainer from './CounterContainer';
 
 function App() {
   return (
     <div>
+          <ShopProvider>
+      <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route path='/' element={<ItemListContainer/>}></Route>
+            <Route path='/category/:categoryId' element={<ItemListContainer/>}></Route>
+            <Route path='/detail/:productId' element={<ItemDetailContainer/>}></Route>
+            <Route path='/cart' element={<Cart/>}></Route>
+            <Route path='*' element={<NotFound/>}/>
+          </Routes>       
+      </BrowserRouter>
+    </ShopProvider>
       <CounterContainer/>
     </div>
   );
